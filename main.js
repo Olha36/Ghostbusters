627f91ee359cd3fa8464d5ec9dc6a9e5fb700ea7
Olha36 <olhakucheruk36@gmail.com>
2022-03-15 08:56:45 +0100

    Responsive

---
+++ main.js
@@ -0,0 +1,6 @@
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.sub_menu_list1');
+
+menuButton.addEventListener('click', function() {
+    console.log('button click');
+});

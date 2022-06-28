85a92e65744842286cc6beb7ad1244d24ab112a9
Olha36 <olhakucheruk36@gmail.com>
2022-06-28 12:11:45 +0200

    Almost fixed

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var menuButton = document.querySelector('.menu-button');
-var menu = document.querySelector('.sub_menu_list1');
+let burgerMenu = document.querySelector('.burger-menu');
+console.log(burgerMenu)
+// let menu = document.querySelector('.sub_menu_list1');
+// console.log(menu);
 
-menuButton.addEventListener('click', function() {
-    console.log('button click');
-});
+if (burgerMenu) {
+    let menu = document.querySelector('.sub_menu_list1');
+
+    burgerMenu.addEventListener("click", function(x) {
+        console.log(burgerMenu.classList.toggle("._active"));
+        console.log(menu.classList.toggle("._active"));
+    })
+}
+
+// burgerMenu.addEventListener('click', function() {
+//     burgerMenu.classList.toggle('.burger-menu-active');
+//     menu.classList.toggle('.sub_menu_list1.active');
+
+// });

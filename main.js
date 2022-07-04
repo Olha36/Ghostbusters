a65ee4ca418f7994db9907d8975ac1eeef2b0ccb
Olha36 <olhakucheruk36@gmail.com>
2022-07-04 10:51:05 +0200

    New changes

--- main.js
+++ main.js
@@ -1,19 +1,10 @@
 let burgerMenu = document.querySelector('.burger-menu');
 console.log(burgerMenu)
-// let menu = document.querySelector('.sub_menu_list1');
-// console.log(menu);
-
-if (burgerMenu) {
-    let menu = document.querySelector('.sub_menu_list1');
-
-    burgerMenu.addEventListener("click", function(x) {
-        console.log(burgerMenu.classList.toggle("._active"));
-        console.log(menu.classList.toggle("._active"));
-    })
-}
+let menu = document.querySelector('.sub_menu_list1');
+console.log(menu);
 
 // burgerMenu.addEventListener('click', function() {
 //     burgerMenu.classList.toggle('.burger-menu-active');
-//     menu.classList.toggle('.sub_menu_list1.active');
+//     menu.classList.toggle('.sub_menu_list1_active');
 
 // });

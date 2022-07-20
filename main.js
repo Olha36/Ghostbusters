977e6bdbe89a408ec68d30d5b3fd6aaa886da10f
Olha36 <olhakucheruk36@gmail.com>
2022-07-20 16:43:41 +0200

    Last edit

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let menuButton = document.querySelector('.menu-btn');
-// console.log(menuButton);
 
 let menuActive = document.querySelector('#menu-active');
 console.log(menuActive);
 
 menuButton.addEventListener('click', function () {
-    menuActive.classList.toggle('menu-btn-active');
-    menuActive.classList.toggle('header_menu');
+    menuActive.classList.toggle('menu')
 })
